a2845c4b19cceb1a0cedb034a9ceb5a44d7a500d
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2023-05-19 16:45:48 -0700

    updates to R script

--- scripts/fix_organisms.R
+++ scripts/fix_organisms.R
@@ -2,6 +2,22 @@
 
 library(yaml)
 
+# Custom yaml writing function
+write_yaml_custom <- function(data, con) {
+  cat("---\n", file = con)
+  for (i in 1:length(data)) {
+    if (names(data[i]) == "organisms") {
+      cat("organisms:\n", file = con, append = TRUE)
+      for (o in data[i]){
+        cat("- ",o, "\n", sep = "", file = con, append = TRUE)
+      }
+    } else {
+      yaml::write_yaml(data[i], con)
+    }
+  }
+  cat("---\n", file = con, append = TRUE)
+}
+
 # Specify the directory containing the YAML files
 directory <- "../_figures"
 
@@ -9,18 +25,23 @@ directory <- "../_figures"
 files <- list.files(directory, pattern = "*.md", full.names = TRUE)
 
 # Process each file
-for (file in files) {
+for (f in files) {
   # Read the YAML data
-  data <- yaml.load_file(file)
-
-  # Check if 'organisms' key exists and is a list
-  if ("organisms" %in% names(data) && is.list(data$organisms)) {
-    # Replace 'plant' with 'unspecified plant'
-    data$organisms <- lapply(data$organisms, function(x) ifelse(x == "plant", "unspecified plant", x))
+  data <- yaml.load_file(f)
+  print(data$figid_alias)
+  
+  # Check if 'organisms' key exists
+  if ("organisms" %in% names(data)) {
+    # If 'organisms' is a list, preserve the list structure
+    if (is.list(data$organisms)) {
+      data$organisms <- lapply(data$organisms, function(x) ifelse(x == "plant", "unspecified plant", x))
+    } else if (data$organisms == "plant") {
+      data$organisms <- "unspecified plant"
+    }
   }
 
   # Write the data back to the file
-  con <- file(file, "w")
+  con <- file(f, "w")
   on.exit(close(con))
-  yaml::write_yaml(data, con)
+  write_yaml_custom(data, con)
 }
